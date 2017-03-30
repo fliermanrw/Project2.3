9b0e1ae84187f2e1bc529e4f82a04f093a386802
jyrienstra <joukerienstra@hotmail.com>
2017-03-30 21:30:22 +0200

    added loggin view, added gameModel interface

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/view/connectedView.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/loginView.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 200, 150));
         primaryStage.show();
     }
 
---
+++ src/model/gameModel.java
@@ -0,0 +1,16 @@
+package model;
+
+/**
+ * Created by jouke on 30-3-2017.
+ * Interface for the game model
+ * Please implement these functions in Boggle & Tic-tac-toe
+ */
+public interface gameModel {
+    boolean isFinished();
+
+    void getCurrentPlayer(); //@todo probably changes the return type
+
+    int[][] getValidMoves();
+
+    void move(int y, int x);
+}
