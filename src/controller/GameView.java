87faee7475ab335c4f1452a8448edf4c6f5cd46a
Ryan Flierman <r.w.flierman@st.hanze.nl>
2017-04-03 15:09:18 +0200

    tictactoe view Grid

---
+++ src/controller/GameView.java
@@ -0,0 +1,10 @@
+package controller;
+
+/**
+ * Created by Ryan on 3-4-2017.
+ */
+public abstract class GameView {
+    abstract void move(int place);
+
+    abstract void forfeit();
+}
--- src/controller/GridController.java
+++ src/controller/GridController.java
@@ -1,10 +1,32 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+
+
 /**
  * Created by jouke on 3-4-2017.
  */
-public class GridController {
-    public void buttonClick(){
 
+public class GridController extends GameView{
+    public void buttonClick(ActionEvent actionEvent) {
+        String x = actionEvent.getSource().toString();
+        System.out.println(x);
+        if(x.equals("b1")){
+            System.out.println("xx");
+        } else System.out.println("yy");
+    }
+
+    @Override
+    void move(int place) {
+        //@todo
+    }
+
+    @Override
+    void forfeit() {
+        //@todo
     }
 }
+
