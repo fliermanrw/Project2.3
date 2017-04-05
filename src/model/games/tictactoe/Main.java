6521493c62036eb9065d50f8ed6792dabbec8297
DDJager <danny-1995@live.nl>
2017-04-05 22:56:07 +0200

    Added the TicTacToe minimax to the Models

---
+++ src/model/games/tictactoe/Main.java
@@ -0,0 +1,12 @@
+package model.games.tictactoe;
+
+/**
+ * Created by Danny on 5-4-2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Tictactoe tictactoe = new Tictactoe();
+    }
+
+}
--- src/model/minimax_los_tijdelijk/Board.java
+++ src/model/minimax_los_tijdelijk/Board.java
@@ -41,8 +41,8 @@ public class Board {
             int pos = Integer.parseInt(scanner.nextLine());
             generateView(pos, humanPlayer);
 
-            Move bestMove = minimax(board, aiPlayer, new Move(0));
-            generateView(bestMove.getIndex(), aiPlayer);
+            //Move bestMove = minimax(board, aiPlayer, new Move(0));
+            //generateView(bestMove.getIndex(), aiPlayer);
         }
 
 //        Move bestMove = minimax(board, aiPlayer, new Move(0));
@@ -122,78 +122,78 @@ public class Board {
      * @param player
      * @return
      */
-    private Move minimax(ArrayList<String> newBoard, String player, Move m) {
-
-        // Increment the depth (or function call) by 1
-        this.depth++;
-        //if (m == null) m = new Move(0);
-        // Get the empty spots
-        ArrayList<String> availableSpots = emptySpots(newBoard);
-
-        // Check who has won. If the availableSpots are 0, the game is a TIE.
-        if (hasWon(newBoard, humanPlayer)) {
-            m.setScore(-10);
-            return m;
-        } else if (hasWon(newBoard, aiPlayer)) {
-            m.setScore(10);
-            return m;
-        } else if (availableSpots.size() == 0) {
-            m.setScore(-1);
-            return m;
-        }
-
-        // Create an ArrayList with Move objects
-         ArrayList<Move> moves = new ArrayList<>();
-
-        // Loop over the available spots, skip the null values
-        for (int i = 0; i < availableSpots.size(); i++) {
-
-            // Create a new Move object and set the available spot as their index
-            Move move = new Move(Integer.parseInt(newBoard.get(Integer.parseInt(availableSpots.get(i)))));
-
-            // Since the moves with index are saved, we can overwrite the spot with the current player
-            newBoard.remove(Integer.parseInt(availableSpots.get(i)));
-            newBoard.add(Integer.parseInt(availableSpots.get(i)), player);
-
-            // Depending on the current player, call minimax() recursively
-            // And save the score in every possible move
-            if (player.equals(aiPlayer)) {
-                Move result = minimax(newBoard, humanPlayer, move);
-                move.setScore(result.getScore());
-            } else {
-                Move result = minimax(newBoard, aiPlayer, move);
-                move.setScore(result.getScore());
-            }
-
-            // Reset the board
-            newBoard.remove(Integer.parseInt(availableSpots.get(i)));
-            newBoard.add(Integer.parseInt(availableSpots.get(i)), Integer.toString(move.getIndex()));
-
-            // Add the move to the ArrayList
-            moves.add(move);
-        }
-
-        int bestMove = 0;
-        if (player.equals(aiPlayer)) {
-            int bestScore = -1000;
-            for(int i = 0; i < moves.size(); i++){
-                if (moves.get(i).getScore() > bestScore) {
-                    bestScore = moves.get(i).getScore();
-                    bestMove = i;
-                }
-            }
-        } else {
-            int bestScore = 1000;
-            for(int i = 0; i < moves.size(); i++){
-                if (moves.get(i).getScore() < bestScore) {
-                    bestScore = moves.get(i).getScore();
-                    bestMove = i;
-                }
-            }
-        }
-
-        return moves.get(bestMove);
-    }
+//    private Move minimax(ArrayList<String> newBoard, String player, Move m) {
+//
+//        // Increment the depth (or function call) by 1
+//        this.depth++;
+//        //if (m == null) m = new Move(0);
+//        // Get the empty spots
+//        ArrayList<String> availableSpots = emptySpots(newBoard);
+//
+//        // Check who has won. If the availableSpots are 0, the game is a TIE.
+//        if (hasWon(newBoard, humanPlayer)) {
+//            m.setScore(-10);
+//            return m;
+//        } else if (hasWon(newBoard, aiPlayer)) {
+//            m.setScore(10);
+//            return m;
+//        } else if (availableSpots.size() == 0) {
+//            m.setScore(-1);
+//            return m;
+//        }
+//
+//        // Create an ArrayList with Move objects
+//         ArrayList<Move> moves = new ArrayList<>();
+//
+//        // Loop over the available spots, skip the null values
+//        for (int i = 0; i < availableSpots.size(); i++) {
+//
+//            // Create a new Move object and set the available spot as their index
+//            Move move = new Move(Integer.parseInt(newBoard.get(Integer.parseInt(availableSpots.get(i)))));
+//
+//            // Since the moves with index are saved, we can overwrite the spot with the current player
+//            newBoard.remove(Integer.parseInt(availableSpots.get(i)));
+//            newBoard.add(Integer.parseInt(availableSpots.get(i)), player);
+//
+//            // Depending on the current player, call minimax() recursively
+//            // And save the score in every possible move
+//            if (player.equals(aiPlayer)) {
+//                Move result = minimax(newBoard, humanPlayer, move);
+//                move.setScore(result.getScore());
+//            } else {
+//                Move result = minimax(newBoard, aiPlayer, move);
+//                move.setScore(result.getScore());
+//            }
+//
+//            // Reset the board
+//            newBoard.remove(Integer.parseInt(availableSpots.get(i)));
+//            newBoard.add(Integer.parseInt(availableSpots.get(i)), Integer.toString(move.getIndex()));
+//
+//            // Add the move to the ArrayList
+//            moves.add(move);
+//        }
+//
+//        int bestMove = 0;
+//        if (player.equals(aiPlayer)) {
+//            int bestScore = -1000;
+//            for(int i = 0; i < moves.size(); i++){
+//                if (moves.get(i).getScore() > bestScore) {
+//                    bestScore = moves.get(i).getScore();
+//                    bestMove = i;
+//                }
+//            }
+//        } else {
+//            int bestScore = 1000;
+//            for(int i = 0; i < moves.size(); i++){
+//                if (moves.get(i).getScore() < bestScore) {
+//                    bestScore = moves.get(i).getScore();
+//                    bestMove = i;
+//                }
+//            }
+//        }
+//
+//        return moves.get(bestMove);
+//    }
 
 }
 
