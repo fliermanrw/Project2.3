aabfce5607c149d8acb3c04d644c43063b87c437
Ryan Flierman <r.w.flierman@st.hanze.nl>
2017-04-06 12:22:09 +0200

    Othellocontroller

--- src/controller/OthelloController.java
+++ src/controller/OthelloController.java
@@ -12,7 +12,6 @@ public class OthelloController extends GameView{
     TelnetWriter connectionWriter;
     Tictactoe tictactoe = new Tictactoe();
     boolean ourturn = false;
-    String committest = "dd";
 
     public void buttonClick(ActionEvent actionEvent) {
         Button btn = (Button) actionEvent.getSource();
