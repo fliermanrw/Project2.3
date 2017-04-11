f680029bbf9664016f8f18e4742daf8bde31406e
jyrienstra <joukerienstra@hotmail.com>
2017-04-11 13:03:28 +0200

    removed unused code in orthello, made main example, added point functionality

--- src/model/games/othello/Main.java
+++ src/model/games/othello/Main.java
@@ -1,13 +1,43 @@
 package model.games.othello;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by jouke on 11-4-2017.
  */
 public class Main {
+    private static othelloGameModel othello;
     public static void main(String args[]){
-        othelloGameModel m = new othelloGameModel('B');
-        othelloBoard board = m.getOthelloBoard();
-        board.recursiveMove();
-        board.printBoard();
+        othello = new othelloGameModel('B');
+        othello.initGrid();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Turn : " + othello.getCurrentPlayer());
+            othello.printBoard();
+            ArrayList<Integer> moves = othello.getValidMoves();
+            System.out.println("Available moves: " + moves);
+            System.out.println("Number of moves: " + moves.size());
+
+            System.out.print("Positie?: ");
+            int pos = Integer.parseInt(scanner.nextLine());
+
+            if(moves.size() == 0){
+                System.out.println("No more moves for: " + othello.getCurrentPlayer());
+                System.out.println("Winner is:" + othello.findCurrentWinner());
+            }else{
+                if(moves.contains(pos)){
+                    othello.move(pos);
+                    othello.switchPlayer();
+                }else{
+                    System.out.println("Wrong move, not an option");
+                }
+            }
+        }
     }
 }
