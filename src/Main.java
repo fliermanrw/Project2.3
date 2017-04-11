6f61ac225bec929c784aa7a03f4bb13c1d242997
jyrienstra <joukerienstra@hotmail.com>
2017-04-11 15:38:50 +0200

    server and port now set trough settings.txt, has default value

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import com.sun.corba.se.spi.activation.Server;
 import controller.LoginController;
 import javafx.application.Application;
 import javafx.event.EventHandler;
@@ -9,13 +8,17 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import model.server_connection.*;
 
-import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main extends Application {
+    public String server = "127.0.0.1"; //default server
+    public Integer port = 7789; //default port
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -28,8 +31,29 @@ public class Main extends Application {
             // Remote
             //TelnetConnection telnet = new TelnetConnection("145.33.225.170", 7789);
 
+            //Read file and change server and port when it is set in the ifle
+            //File should be format key: "value"
+            BufferedReader br = new BufferedReader(new FileReader(new File("settings.txt").getAbsoluteFile()));
+            String currentLine;
+            Map<String, String> vars = new HashMap<>();
+            while ((currentLine = br.readLine()) != null) {
+                Pattern pattern = Pattern.compile("([A-Za-z]+): \"([^\"]*)\"");
+                Matcher matcher = pattern.matcher(currentLine);
+                while (matcher.find()) {
+                    vars.put(matcher.group(1), matcher.group(2));
+                }
+            }
+            System.out.println(vars);
+            if(vars.get("server") != null){
+                this.server = vars.get("server");
+                System.out.printf("??");
+            }
+            if(vars.get("port") != null){
+                this.port = Integer.valueOf(vars.get("port"));
+            }
+
             // Local
-            TelnetConnection telnet = new TelnetConnection("0.0.0.0", 7789);
+            TelnetConnection telnet = new TelnetConnection(server, port);
 
             ServerHandler serverHandler = new ServerHandler(telnet.getConnectionSocket(), primaryStage);
             LoginController loginController = fxmlLoader.getController();
