c2892d830ba8c3dc1ef015205cb8a715019cb516
jyrienstra <joukerienstra@hotmail.com>
2017-04-12 11:54:12 +0200

    Reader/writer logic split, 30% cpu improvement

--- src/model/server_connection/ServerHandlerWriter.java
+++ src/model/server_connection/ServerHandlerWriter.java
@@ -1,12 +1,35 @@
 package model.server_connection;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 /**
  * Created by jouke on 6-4-2017.
  */
 public class ServerHandlerWriter{
+    Socket socket;
+    static BufferedWriter writer;
+
+    public ServerHandlerWriter(Socket s){
+        this.socket = s;
+        try {
+            writer= new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void writeSend(String data){
         ServerHandler.queue.add(data);
+        try {
+            writer.write(data);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void login(String playerName) {
