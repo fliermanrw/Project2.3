6f8e8e59fc51eace13ec60584e692412d8b45d6e
Ryan Flierman <r.w.flierman@st.hanze.nl>
2017-04-13 16:26:13 +0200

    Get the files from the txt file. Moved from main to telnetconnection

--- src/model/server_connection/TelnetConnection.java
+++ src/model/server_connection/TelnetConnection.java
@@ -1,6 +1,11 @@
 package model.server_connection;
 
+import java.io.*;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by jouke on 30-3-2017.
@@ -9,6 +14,7 @@ public class TelnetConnection{
     String ip;
     int port;
     Socket socket;
+
 
     public TelnetConnection(String ip, int port) throws Exception{
         this.ip = ip;
@@ -24,4 +30,26 @@ public class TelnetConnection{
     public Socket getConnectionSocket(){
         return socket;
     }
+
+    public static Map<String, String> getServerFromTxt() throws IOException {
+        //Read file and change server and port when it is set in the ifle
+        //File should be format key: "value"
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(new File("settings.txt").getAbsoluteFile()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String currentLine;
+        Map<String, String> vars = new HashMap<>();
+        while ((currentLine = br.readLine()) != null) {
+            Pattern pattern = Pattern.compile("([A-Za-z]+): \"([^\"]*)\"");
+            Matcher matcher = pattern.matcher(currentLine);
+            while (matcher.find()) {
+                vars.put(matcher.group(1), matcher.group(2));
+            }
+        }
+        return vars;
+    }
 }
+
